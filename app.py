b669eaa5b3b3fde8913f4fa99c20037e0334c8d7
Kevin Weingarten <42475945+kweino@users.noreply.github.com>
2021-10-15 11:57:15 -0700

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template, request, redirect, url_for
 from forms import StockForm
 from analysis import stockAnalysis
 
+from dotenv import dotenv_values
 
+config = dotenv_values(".env")
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'Humb07dt$@ve$TheD@y!'
-
+app.config['SECRET_KEY'] = config['SECRET_KEY']
 
 @app.route('/', methods=['GET','POST'])
 def index():
